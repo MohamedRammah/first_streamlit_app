ee825dff5dceb408e75a907e2255ea33f0c5a6d2
MohamedRammah <83248863+MohamedRammah@users.noreply.github.com>
2022-07-18 10:54:23 +0200

    Update streamlit_app.py
    Import list from AWS 
    display imported data

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,7 @@ streamlit.text ('☘️Hard-boiled Free-Range Egg')
 
 streamlit.header ('🥝 🍓  Create Your Own Milkshake 🍒 🥥')
 
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
