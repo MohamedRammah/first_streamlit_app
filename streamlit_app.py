ce140560662cbd544e667d39388cde0a623b41a6
MohamedRammah <83248863+MohamedRammah@users.noreply.github.com>
2022-07-25 14:15:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,13 +34,5 @@ streamlit.dataframe(my_fruit_list)
 
 
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-uit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
 
 import snowflake.connector
